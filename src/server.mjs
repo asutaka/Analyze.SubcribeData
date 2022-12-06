b21bd373e7e7b477e70f97bf00eeb704267b21a7
phunv@ftech.ai <phunv@ftech.ai>
2022-12-06 22:23:53 +0700

    add mongoose

--- src/server.mjs
+++ src/server.mjs
@@ -8,8 +8,29 @@ app.get('/', async (req, res)  => {
 })
 
 app.listen(3000, () => console.log('server running!'));
+
 // getServerTime().then(data => console.log(data));
+
 // const socketClient = new SocketClient('ws/!miniTicker@arr');
 // socketClient.setHandler('depthUpdate', (params) => console.log(JSON.stringify(params)));
 
 
+import mongoose from 'mongoose'
+const URL = 'mongodb+srv://asutaka:ZLlbiAccbRTwGXpr@datadb0.svcfiir.mongodb.net/?retryWrites=true&w=majority'
+
+const connectDB = async () => {
+  try {
+    mongoose.set('strictQuery', true);
+    await mongoose.connect(
+      URL,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    console.log('Connected to mongoDB');
+    mongoose.getCollection('hello').exists()
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+connectDB()
