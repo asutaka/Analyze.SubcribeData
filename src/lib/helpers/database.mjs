b53af36096b5d5d0dbd869575bc9c7cb964d5360
phunv@ftech.ai <phunv@ftech.ai>
2022-12-08 16:52:46 +0700

    add API data

--- src/lib/helpers/database.mjs
+++ src/lib/helpers/database.mjs
@@ -1,8 +1,16 @@
-import mongoose, { isObjectIdOrHexString } from 'mongoose'
+import mongoose from 'mongoose'
 
 const URL = 'mongodb+srv://asutaka:ZLlbiAccbRTwGXpr@datadb0.svcfiir.mongodb.net/?retryWrites=true&w=majority'
 const conn = async () => {
     try {
+        //0: disconnected
+        //1: connected
+        //2: connecting
+        //3: disconnecting
+        console.log(mongoose.connection.readyState);
+        if(mongoose.connection.readyState == 1)
+            return;
+
         mongoose.set('strictQuery', true);
         await mongoose.connect(
         URL,
@@ -17,132 +25,91 @@ const conn = async () => {
 };
 
 const create = (name) => {
-    checkConnection((callback) => {
-        if(!callback){
-            conn();
-        }
-
-        mongoose.connection.createCollection(name, function (err, res) {
-            if (err) throw err;
-            console.log("collection " + name + " created!");
-        });
+    conn();
+    mongoose.connection.createCollection(name, function (err, res) {
+        if (err) throw err;
+        console.log("collection " + name + " created!");
     });
 };
 
 const findOne = (table) =>{
-    checkConnection((callback) => {
-        if(!callback){
-            conn();
+    conn();
+    mongoose.connection.collection(table).findOne({}, { sort: { _id: -1 } }, function(err, res){
+        if(err){
+            console.log(err);
         }
-        
-        mongoose.connection.collection(table).findOne({}, { sort: { _id: -1 } }, function(err, res){
-            if(err){
-                console.log(err);
-            }
-            else{
-                console.log(res);
-            }
-        });
+        else{
+            console.log(res);
+        }
     });
 };
 
 const find = (table, from, to, callback) =>{
-    checkConnection((callback) => {
-        if(!callback){
-            conn();
+    conn();
+    mongoose.connection.collection(table).find({}, function(err, res){
+        if(err){
+            console.log(err);
         }
-        
-        mongoose.connection.collection(table).find({}, function(err, res){
-            if(err){
-                console.log(err);
-            }
-            else{
-                return callback(res);
-            }
-        });
-
-        // mongoose.connection.collection(table).find({$and: [{ T: { $gt: from } }, { T: { $lt: to } }] }, function(err, res){
-        //     if(err){
-        //         console.log(err);
-        //     }
-        //     else{
-        //         res.foreach(function(tmp){
-        //             console.log(tmp);
-        //         })
-        //     }
-        // });
+        else{
+            return callback(res);
+        }
     });
+
+    // mongoose.connection.collection(table).find({$and: [{ T: { $gt: from } }, { T: { $lt: to } }] }, function(err, res){
+    //     if(err){
+    //         console.log(err);
+    //     }
+    //     else{
+    //         res.foreach(function(tmp){
+    //             console.log(tmp);
+    //         })
+    //     }
+    // });
 };
 
 const addRecord = (table, e, c, o, h, l, v, q) => {
-    checkConnection((callback) => {
-        if(!callback){
-            conn();
-        }
-        
-        var dat = {
-            T : e,
-            C : c,
-            O : o,
-            H : h,
-            L : l,
-            V : v,
-            Q : q
-        };
+    conn();
+    var dat = {
+        T : e,
+        C : c,
+        O : o,
+        H : h,
+        L : l,
+        V : v,
+        Q : q
+    };
 
-        mongoose.connection.collection(table).insertOne(dat);
-    });
+    mongoose.connection.collection(table).insertOne(dat);
 };
 
 const updateRecord = (table, id, e, c, o, h, l, v, q) =>{
-    checkConnection((callback) => {
-        if(!callback){
-            conn();
+    conn();
+    mongoose.connection.collection(table).findOneAndUpdate({ _id: -1 }, { T : e, C : c, O : o, H : h, L : l, V : v, Q : q }, {new: true}, function(err, res){
+        if(err){
+            console.log(err);
         }
-        
-        mongoose.connection.collection(table).findOneAndUpdate({ _id: -1 }, { T : e, C : c, O : o, H : h, L : l, V : v, Q : q }, {new: true}, function(err, res){
-            if(err){
-                console.log(err);
-            }
-            else{
-                console.log(res);
-            }
-        });
+        else{
+            console.log(res);
+        }
     });
 };
 
 const deleteRecord = (table, id) =>{
-    checkConnection((callback) => {
-        if(!callback){
-            conn();
+    if(id == null)
+    {
+        console.log("id is null");
+        return;
+    }
+    conn();
+    mongoose.connection.collection(table).findOneAndDelete({_id: id}, function(err, res){
+        if(err){
+            console.log(err);
         }
-
-        if(id == null)
-        {
-            console.log("id is null");
-            return;
+        else{
+            console.log("Deleted Record:", id);
+            console.log(res);
         }
-        
-        mongoose.connection.collection(table).findOneAndDelete({_id: id}, function(err, res){
-            if(err){
-                console.log(err);
-            }
-            else{
-                console.log("Deleted Record:", id);
-                console.log(res);
-            }
-        });
     });
 };
-
-
-
-function checkConnection(callback) {
-    //0: disconnected
-    //1: connected
-    //2: connecting
-    //3: disconnecting
-    return callback(mongoose.connection.readyState == 1);
-}
 
 export default {conn, create, findOne, find, addRecord, updateRecord, deleteRecord};
--- src/server.mjs
+++ src/server.mjs
@@ -1,8 +1,10 @@
 import getServerTime from './lib/services/getServerTime.mjs';
 import SocketClient from './lib/socketClient.mjs';
 import db from './lib/helpers/database.mjs';
+import mongoose from 'mongoose'
 import express from "express";
 const app = express();
+const connection = mongoose.connection;
 
 app.get('/', async (req, res)  => {
     res.status(200).json({msg: "hello world" });
@@ -10,8 +12,18 @@ app.get('/', async (req, res)  => {
 
 app.listen(3000, () => console.log('server running!'));
 
-// db.findOne("C98USDT");
-// db.find("C98USDT", 1,11, callback);
+await db.conn();
+
+
+app.get('/data', function(req, res) {
+    const collection  = connection.db.collection("C98USDT");
+    var promise = collection.find({}).toArray();
+    promise.then(function(contacts){
+        res.status(200).json({data: contacts }); 
+        // console.log(contacts);
+    })
+});
+
 
 
 
@@ -20,3 +32,13 @@ app.listen(3000, () => console.log('server running!'));
 
 // const socketClient = new SocketClient('ws/!miniTicker@arr');
 // socketClient.setHandler('depthUpdate', (params) => console.log(JSON.stringify(params)));
+
+// app.get('/test', function(req, res) {
+//     connection.collection("C98USDT").findOne({}, { sort: { _id: -1 } }, function(err, result){
+//         if(err){
+//             console.log(err);
+//             res.status(200).json({data: null });  
+//         }
+//         res.status(200).json({data: result });  
+//     });
+// });
